15ad23a032b8d2302c0b3e06bdf22419ef81055c
Barış Zıbahalat Mofrad <mofrad@outlook.com.tr>
2022-04-07 11:22:16 +0300

    Proje dosyası ekle.

---
+++ Nesne-Tabanli-Programlama-OOP-Projesi-Carpim-Tablosu/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nesne_Tabanli_Programlama_OOP_Projesi_Carpim_Tablosu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(comboBox1.SelectedItem);
+            for (int i = 1; i <= 10; i++)
+            {
+                int toplam = sayi * i;
+                listBox1.Items.Add(sayi + " x " + i + " = "+toplam.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+        }
+    }
+}
